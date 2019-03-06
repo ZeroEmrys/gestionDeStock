285de1e516e81ff10d348e06dc6d7fd585eb9e16
ZeroEmrys <siegfried9994@gmail.com>
2019-03-06 17:03:53 +0300

    all2

--- back-nest/src/app.controller.ts
+++ back-nest/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('rjTest2')
+@Controller('rjTe')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
